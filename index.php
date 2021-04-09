d339da98125864a6495b1abf5fe3b1175d82258c
maqamylee0 <maqamylee0@gmail.com>
2021-04-10 01:43:03 +0300

    header

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<title>Notes</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
 <?php require_once 'includes/header.php';?>
 
 <h1 id="stack">Stack</h1>
