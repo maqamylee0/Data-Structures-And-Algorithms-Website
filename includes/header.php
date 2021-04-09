acc033388dda4b670341339d93c23cd8564d36c9
maqamylee0 <maqamylee0@gmail.com>
2021-04-10 00:40:43 +0300

    initial

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<title>Notes</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+<div id="centrecontainer">
+  <div id="outercontainer">
+    <div id="container">
+      <div id="headercont">
+        <ul>
+          <li><a href="#">valid xhtml</a></li>
+          <li>|</li>
+          <li><a href="#">valid css</a></li>
+        </ul>
+      </div>
+      <div id="maincont">
+        <div id="mainleft">
